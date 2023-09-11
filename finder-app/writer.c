5779515685fb4acf13386491aa519e60f0172e02
Nicholas-Buckley77 <nibu7565@colorado.edu>
2023-09-10 19:53:08 -0600

    added writer, makefile, and txt files proving cross-compile working! Also fixed finder-test for assignment 2 (works with c compiled app)

---
+++ finder-app/writer.c
@@ -0,0 +1,48 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <syslog.h>
+#include <string.h>
+
+// Code skeleton made from chat GPT-4 "a c program that takes a filename and a text string and makes the file with the string as it's contents also setting up and using syslog with LOG_DEBUG"
+
+// Function to create file and write a string to it
+void createFileWithString(char *filename, char *text) {
+
+    // make file pointer to write to
+    FILE *fp;
+    fp = fopen(filename, "w");
+    
+    
+    if (fp == NULL) {
+        syslog(LOG_ERR, "Error opening file %s", filename);
+        return;
+    }
+    
+    // print text to file
+    fputs(text, fp);
+    fclose(fp);
+}
+
+// main program that get's command line arguments
+int main(int argc, char* argv[]) {
+    
+    if(argc != 3)
+    {
+    	syslog(LOG_ERR, "%d is the incorrect number of arguments, needs a filename and string to be written to the file", argc);
+    	return 1;
+    
+    }
+    
+    // setup system log with PID and CONS and at priorty USER
+    openlog("NB Writer", LOG_PID | LOG_CONS, LOG_USER);
+    
+    // log the described debug message
+    syslog(LOG_DEBUG, "Writing %s to %s where %s is a text string written to the file (second argument) and %s is the file created by the script.",argv[2], argv[1], argv[2], argv[1] );
+    
+    createFileWithString(argv[1], argv[2]);
+    
+    // close and cleanup logging
+    closelog();
+    return 0;
+}
